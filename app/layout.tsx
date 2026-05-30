97a81e05d91af5af27ddf60d58049da81f67db32
Tu Nombre Real <tu-email-real@loquesea.com>
2026-05-30 21:25:10 +0000

    fix: agregar tabler icons font, iconos en menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css" />
         <meta
           name="description"
           content="Super Juampy ofrece panificados frescos, fiambrería y productos de supermercado en Charata, Chaco."
